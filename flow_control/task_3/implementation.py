9f0482c82aa2fa708ae72b2fb6ca83e39295ba4c
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-02 20:46:32 +0300

    Update implementation.py
    Задание выполнено

--- flow_control/task_3/implementation.py
+++ flow_control/task_3/implementation.py
@@ -4,4 +4,21 @@ def get_days_count_by_month(month):
         month: название месяца
     Returns: количество дней
     """
-    raise NotImplementedError
+    if month.capitalize() in ['Январь', 'Март', 'Май', 'Июль', 'Август', 'Октябрь', 'Декабрь']:
+        print(31)
+    elif month.capitalize() in ['Апрель', 'Июнь', 'Сентябрь', 'Ноябрь']:
+        print(30)
+    elif month.capitalize() =='Февраль':
+        print(28)
+    else:
+        print(0)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
