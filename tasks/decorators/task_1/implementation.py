58324e95e51ded2949aa02ff80df3f6dfa3fb58c
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-02 23:04:45 +0300

    Update implementation.py

--- tasks/decorators/task_1/implementation.py
+++ tasks/decorators/task_1/implementation.py
@@ -11,3 +11,18 @@ def time_execution(function):
         return final_result
     return wrapper
     
+@time_execution
+def factorial(number):
+    """
+    Возвращает факториал переданного числа
+    Args:
+        number: число, для которого надо посчитать факториал
+    Returns:
+        product - int - факториал от number
+    """
+    product = 1
+    for element in range(1, number+1):
+        product *= element
+
+    return product
+print(factorial(10000))
