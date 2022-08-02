1a17add7f613b3843209b8c57df55e8f1cab4118
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-02 20:50:54 +0300

    Update question.py
    Задание выполнено

--- scope/task_2/question.py
+++ scope/task_2/question.py
@@ -12,3 +12,10 @@ def transmit_to_space(message):
 
 
 print(transmit_to_space("Test message"))
+"""
+После выполнения кода будет выведено:
+Test message
+None - так как функция data_transmitter() не принимает на вход параметры,
+это не задано в ее описании.
+
+"""
