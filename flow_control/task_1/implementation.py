f2dbee4b3174e198b93e03e77e84226e72c3f1c7
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-02 20:39:40 +0300

    Update implementation.py

--- flow_control/task_1/implementation.py
+++ flow_control/task_1/implementation.py
@@ -2,4 +2,10 @@ def get_numbers():
     """Возвращает все числа от 1000 до 2000, которые делятся на 7, но не кратны 5
     Returns: итерируемый объект с нужными числами
     """
+    L=[]
+    for i in range(1000, 2000):
+        if i%7==0 and i%5!=0:
+            L.append(i)
+            
+    print(L)
     raise NotImplementedError
