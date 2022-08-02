489f1ef3a8450961445cbc30a30100dbd7511a73
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-03 02:05:49 +0300

    Update implementation.py

--- tasks/decorators/task_2/implementation.py
+++ tasks/decorators/task_2/implementation.py
@@ -4,4 +4,29 @@ def check_value():
     В случае валидного значения - передает дальше в функцию,
     в противном случае - выбрасывает исключение MyException.
     """
-    raise NotImplementedError
+    def wrapper(number):
+        if type(number) is int and number>0:
+            base_result=function(number)
+            return base_result
+            
+        else:
+            print('В функцию передано невалидное значение')
+    return wrapper
+
+"""
+Кэширование результата на примере моей функции
+"""
+from functools import lru_cache
+
+@lru_cache(maxsize=30)
+def foo(number):
+    s=number+1
+    return s
+print(foo(1000))
+
+    
+    
+    
+    
+    
+    
