03b0c0b947f2ea31e637a4216cee259cc0bfbb4c
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-03 14:52:48 +0300

    Update implementation.py

--- tasks/decorators/task_3/implementation.py
+++ tasks/decorators/task_3/implementation.py
@@ -1,8 +1,18 @@
+
+count =0
 def counter():
     """
     Обертка для подсчёта количества вызовов обернутой функции.
     Returns:
         int - количество вызовов функции.
     """
+    def wrapper():
+        global count
+        base_result=function()
+        count+=1
+        return count
+    
+    return wrapper
+    print(count)
 
-    raise NotImplementedError
+    
