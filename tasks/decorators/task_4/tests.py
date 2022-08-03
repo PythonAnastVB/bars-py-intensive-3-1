115beb6b2a680019ff66005c78d124361d5f0d6e
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-03 16:01:58 +0300

    Update tests.py

--- tasks/decorators/task_4/tests.py
+++ tasks/decorators/task_4/tests.py
@@ -1,20 +1,20 @@
 import unittest
 
 from tasks.common import specific_func, MyException
-from tasks.decorators.task_4.implementation import decorator_maker
+from tasks.decorators.task_4.implementation import decorator_maker_function
 
 
 class MyTestCase(unittest.TestCase):
     def test_on_getting_right_value(self):
-        new_factorial = decorator_maker(3, 0)(specific_func)
+        new_factorial = decorator_maker_function(3, 0)(specific_func)
         self.assertEqual(new_factorial(), 1)
 
     def test_on_getting_incorrect_Value(self):
-        new_factorial = decorator_maker(4, 0)(specific_func)
+        new_factorial = decorator_maker_function(4, 0)(specific_func)
         self.assertEqual(new_factorial(), 1)
 
     def test_on_no_getting_value(self):
-        new_factorial = decorator_maker(2, 0)(specific_func)
+        new_factorial = decorator_maker_function(2, 0)(specific_func)
         self.assertRaises(MyException, new_factorial)
 
 
