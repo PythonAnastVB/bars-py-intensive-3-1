551d89f427e0b0268d54b65cb7d2f78c509d5cc8
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-14 20:12:13 +0300

    Update implementation.py

--- day_5_tasks/context_manager/implementation.py
+++ day_5_tasks/context_manager/implementation.py
@@ -3,3 +3,12 @@
 сначала всегда по умолчанию печатает количество строк в этом файле.
 Можно использовать класс или декоратор @contextmanager
 """
+@contextmanager
+def open_file(name):
+    f=open(name, w)
+    count=0
+    for line in f:
+        count+=1
+    yield f
+    yield count
+    f.close
