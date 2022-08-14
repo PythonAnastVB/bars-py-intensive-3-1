f7662b34b0c2c05473014f25ecd9e35481ef4732
PythonAnastVB <110056710+PythonAnastVB@users.noreply.github.com>
2022-08-14 20:03:41 +0300

    Update implementation.py

--- day_5_tasks/generators/task_2/implementation.py
+++ day_5_tasks/generators/task_2/implementation.py
@@ -2,3 +2,10 @@
 Представим, что у нас есть огромный файл log.txt.
 Нужно вернуть все строки файла, которые содержат слово „error“
 """
+import io
+
+word=u'error'
+with io.open('log.txt', encoding='utf-8') as file:
+    for line in file:
+        if word in line:
+            print(line, and='')
